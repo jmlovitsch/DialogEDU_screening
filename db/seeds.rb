b28da698a591e5adfcfc3582118a9f98898de0bb
john <jmlovitsch@gmail.com>
2021-02-25 14:20:03 -0600

    faker seed data added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+10.times do
+    Comment.create(content: Faker::Lorem.sentences(number: 5, supplemental: true), article_id: Article.create(title: Faker::Lorem.words(number: 4), body: Faker::Lorem.paragraphs(number: 20) ).id )
+
+end
